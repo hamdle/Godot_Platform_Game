69ef22df5e1f5396f6c80b09af805679488f79d0
hamdle <contact@ericmarty.co>
2018-05-23 11:34:14 -0700

    Finish move controls and add collisions

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -3,11 +3,16 @@ extends KinematicBody2D
 var motion = Vector2()
 
 func _physics_process(delta):
+	# Simple gravity
+	motion.y += 10
 	
-	if Input.is_action_just_pressed("ui_right"):
+	# Controls
+	if Input.is_action_pressed("ui_right"):
 		motion.x = 100
-	if Input.is_action_just_pressed("ui_left"):
+	elif Input.is_action_pressed("ui_left"):
 		motion.x = -100
+	else:
+		motion.x = 0
 		
 	move_and_slide(motion)
 	
