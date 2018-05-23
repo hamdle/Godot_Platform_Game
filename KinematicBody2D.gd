e69fac056190b9fe60a3f72f40dbc9b6c5a08cc3
hamdle <contact@ericmarty.co>
2018-05-23 11:48:38 -0700

    Add jump and const to script

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,19 +1,28 @@
 extends KinematicBody2D
 
+const UP = Vector2(0, -1)
+const GRAVITY = 20
+const SPEED = 200
+const JUMP_HEIGHT = -550
+
 var motion = Vector2()
 
 func _physics_process(delta):
 	# Simple gravity
-	motion.y += 10
+	motion.y += GRAVITY
 	
 	# Controls
 	if Input.is_action_pressed("ui_right"):
-		motion.x = 100
+		motion.x = SPEED
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -100
+		motion.x = -SPEED
 	else:
 		motion.x = 0
 		
-	move_and_slide(motion)
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = JUMP_HEIGHT
 	
+	motion = move_and_slide(motion, UP)
+
 	pass
